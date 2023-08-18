5d65c7a88c40e53c6d0d740b0a7c07844870a8b8
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2023-08-18 10:07:15 -0700

    Add website (#2)
    * Add website
    
    * Creates a service network
    * Creates the website pod
    
    * Fix providers in some resources
    
    * Fix provider role
    
    The role should be the `-admin` because it creates resources

---
+++ data_sources.tf
@@ -0,0 +1,31 @@
+data "aws_availability_zones" "uw1" {
+  provider = aws.aws-uw1
+  state    = "available"
+}
+
+data "aws_ami" "ubuntu_22" {
+  provider    = aws.aws-uw1
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+data "aws_key_pair" "aleks" {
+  provider = aws.aws-uw1
+  key_name = "aleks"
+}
---
+++ main.tf
@@ -0,0 +1,32 @@
+module "website" {
+  providers = {
+    aws = aws.aws-uw1
+  }
+  source                = "infrahouse/website-pod/aws"
+  version               = "~> 0.1"
+  ami                   = data.aws_ami.ubuntu_22.image_id
+  backend_subnets       = module.website-vpc.subnet_private_ids
+  dns_zone              = "infrahouse.com"
+  internet_gateway_id   = module.website-vpc.internet_gateway_id
+  key_pair_name         = data.aws_key_pair.aleks.key_name
+  subnets               = module.website-vpc.subnet_public_ids
+  userdata              = module.webserver_userdata.userdata
+  webserver_permissions = data.aws_iam_policy_document.webserver_permissions.json
+}
+
+module "webserver_userdata" {
+  providers = {
+    aws = aws.aws-uw1
+  }
+  source      = "infrahouse/cloud-init/aws"
+  version     = "~> 1.1, >= 1.1.1"
+  environment = var.environment
+  role        = "webserver"
+}
+
+data "aws_iam_policy_document" "webserver_permissions" {
+  statement {
+    actions   = ["ec2:Describe*"]
+    resources = ["*"]
+  }
+}
