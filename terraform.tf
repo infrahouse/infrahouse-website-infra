e2cbe1ee6793143f63c6596d7715134a55d16d47
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2024-12-01 10:17:26 -0800

    Prepare for TF 1.10.0 (#31)

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,12 @@
 terraform {
   backend "s3" {
-    bucket         = "infrahouse-website-infra"
-    key            = "terraform.tfstate"
-    region         = "us-west-1"
-    role_arn       = "arn:aws:iam::289256138624:role/ih-tf-infrahouse-website-infra-state-manager"
+    bucket = "infrahouse-website-infra"
+    key    = "terraform.tfstate"
+    region = "us-west-1"
+    assume_role = {
+      role_arn = "arn:aws:iam::289256138624:role/ih-tf-infrahouse-website-infra-state-manager"
+    }
+
     dynamodb_table = "infrahouse-terraform-state-locks"
     encrypt        = true
   }
