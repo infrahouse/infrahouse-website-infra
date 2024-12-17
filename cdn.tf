f9378dd58de38ca711ca805e73509844fd865a53
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2024-12-16 17:55:01 -0800

    Deploy stagin CDN

--- cdn.tf
+++ cdn.tf
@@ -6,3 +6,12 @@ module "cdn" {
   }
   cdn_hostname = "cdn"
 }
+
+module "cdn-staging" {
+  source = "./modules/cdn"
+  providers = {
+    aws     = aws.aws-uw1
+    aws.ue1 = aws.aws-ue1
+  }
+  cdn_hostname = "cdn-staging"
+}
