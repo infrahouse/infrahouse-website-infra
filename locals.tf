08c88ad83587d9172a40320bc11fdfa90ecd69dd
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2026-05-29 11:48:16 -0700

    Add us-east-1 as a DR region

--- locals.tf
+++ locals.tf
@@ -2,4 +2,5 @@ locals {
   ubuntu_codename      = "noble"
   ami_name_pattern_pro = "ubuntu-pro-server/images/hvm-ssd-gp3/ubuntu-${local.ubuntu_codename}-*"
   cdn_logs_bucket_name = "infrahouse-cdn-access-logs"
+  dr_region            = "us-east-1"
 }
--- www.tf
+++ www.tf
@@ -10,13 +10,12 @@ module "website" {
   backend_subnets              = data.aws_subnets.management_private.ids
   zone_id                      = data.aws_route53_zone.infrahouse_com.zone_id
   dns_a_records                = ["", "www"]
-  internet_gateway_id          = data.aws_internet_gateway.management.id
+  replication_region           = local.dr_region
   key_pair_name                = data.aws_key_pair.aleks.key_name
   subnets                      = data.aws_subnets.management_public.ids
   userdata                     = module.webserver_userdata.userdata
   instance_profile_permissions = data.aws_iam_policy_document.webserver_permissions.json
   stickiness_enabled           = true
-  alb_access_log_enabled       = true
   on_demand_base_capacity      = 1
 }
 
